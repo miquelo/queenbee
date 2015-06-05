c52152203f0bb6fda2445053a9eb8a3a51025620
Miquel A. Ferran <miquel.ferran@infojobs.net>
2015-06-05 14:30:15 +0200

    Remote Docker fix in Debian 8.

--- layout/image/debian-docker/scripts/docker.sh
+++ layout/image/debian-docker/scripts/docker.sh
@@ -3,7 +3,11 @@
 curl -sSL https://get.docker.com/ | sh
 
 # Configure remote access
-sed -i "s/DOCKER_OPTS=$/DOCKER_OPTS=\'-H tcp:\/\/0.0.0.0:4243 -H unix:\/\/\/var\/run\/docker.sock\'" /etc/init/docker.conf
+sed -i "s/^#DOCKER_OPTS=\".*\"$/DOCKER_OPTS=\"-H tcp:\/\/0.0.0.0:4243\"" /etc/default/docker
+
+# Fix docker service to use DOCKER_OPTS
+# http://stackoverflow.com/questions/30127580/docker-opts-in-etc-default-docker-ignored-on-debian8
+systemctl daemon-reload
 
 # Non-root access
 gpasswd -a ${USER} docker
