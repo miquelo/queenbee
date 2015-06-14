2cb45ec24d9d6e0988bd68c1f7ca2df0245144bb
Miquel Ferran <miquel.ferran.gonzalez@gmail.com>
2015-06-14 23:55:17 +0200

    Docker registry and executor 01.

--- layout/stack/docker/setup.sh
+++ layout/stack/docker/setup.sh
@@ -16,5 +16,30 @@
 # along with QueenBee Project.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# https://blog.docker.com/2013/07/how-to-use-your-own-registry/
+# Docker repository init script 
+echo -e "#!/bin/bash
+### BEGIN INIT INFO
+# Provides:				docker-repo
+# Required-Start:		\$local_fs \$network
+# Required-Stop:		\$local_fs
+# Default-Start:		2 3 4 5
+# Default-Stop:			0 1 6
+# Short-Description:	Docker Repository
+# Description:			Repository for Docker images
+### END INIT INFO
+
+case \"\$1\" in
+	start)
+	docker run -d -p 5000:5000 --restart=always --name registry registry
+	;;
+	stop)
+	docker stop --name registry
+	;;
+esac
+" > /etc/init.d/docker-repo
+chmod 755 /etc/init.d/docker-repo
+update-rc.d docker-repo defaults
+
+# Running Docker repository at provision time
+/etc/init.d/docker-repo start
 
